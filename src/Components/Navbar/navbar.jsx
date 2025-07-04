e5002fbcaf47d22039f5aaafd64b6a6e966798ea
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-07-05 01:24:44 +0300

    Update Navbar component with gradient direction change and fix logo image placement for improved layout consistency.

--- src/Components/Navbar/navbar.jsx
+++ src/Components/Navbar/navbar.jsx
@@ -9,7 +9,7 @@ export default function navbar() {
   <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
   <div className="drawer-content flex flex-col">
     {/* Navbar */}
-    <div className="navbar   fixed top-0 z-2 bg-gradient-to-tr  from-[rgba(0,0,0,0.5)] to-[#0c3e36] text-white">
+    <div className="navbar   fixed top-0 z-2 bg-gradient-to-br  from-[rgba(0,0,0,0.5)] to-[#0c3e36] text-white">
       <div className="flex-none lg:hidden">
         <label htmlFor="my-drawer-3" aria-label="open sidebar" className="btn btn-square btn-ghost">
           <svg
@@ -29,9 +29,9 @@ export default function navbar() {
       </div>
       <div className="mx-2 flex-1 px-2">
          <a href='/#' 
-        className=' text-2xl  md:text-4xl flex content-center items-center '> Sp<img data-aos="fade-down" data-aos-duration="1000"
+        className=' text-2xl  md:text-4xl flex content-center items-center '> <img data-aos="fade-down" data-aos-duration="1000"
          data-aos-delay="500"
-         src={logo} className=' md:h-[70px] md:w-[70px] h-[50px] w-[50px] '    alt="logo"/>der</a> 
+         src={logo} className=' md:h-[70px] md:w-[70px] h-[50px] w-[50px] '    alt="logo"/></a> 
         </div>
       <div className="hidden flex-none lg:block ">
         <ul className="menu menu-horizontal  text-white">
