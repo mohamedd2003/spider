6a4f79a35f6f562dce63b75f831869f828e90c6b
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-07-04 23:13:39 +0300

    Add smooth scrolling functionality to Home component based on URL hash for improved navigation experience.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Hero from '../../Components/Hero/Hero'
 import About from '../../Components/About/About'
 import Services from '../../Components/Services/Services'
 import Clients from '../../Components/Clients/clients'
 import Contact from '../../Components/Contact/contact'
 import OurWork from '../../Components/OurWork'
+import { useLocation } from 'react-router-dom'
 
 export default function Home() {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (location.hash) {
+      const element = document.querySelector(location.hash);
+      if (element) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' });
+        }, 100); // delay ensures the DOM is rendered
+      }
+    }
+  }, [location]);
   return (
    <>
 <Hero/>
