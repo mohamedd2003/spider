585fa28b8adc61be10588f5432a785c6bd69d893
mohamedd2003 <105282528+mohamedd2003@users.noreply.github.com>
2025-07-05 04:58:44 +0300

    Update About component to reflect the correct founding year of the Spider Marketing Agency and enhance service descriptions for clarity and engagement.

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -6,21 +6,28 @@ export default function About() {
  
   <section   className="overflow-hidden  sm:grid sm:grid-cols-2 sm:items-center lato">
   <div className="p-8 md:p-12 lg:px-16 lg:py-24">
-    <div className="mx-auto max-w-xl text-center ltr:sm:text-left rtl:sm:text-right">
+    <div className="mx-auto max-w-xl  ltr:sm:text-left ">
       <h2 className="text-xl font-bold text-white md:text-3xl">
-         <span className='text-[#125e51]'>Spider</span> Marketing Agency Established in 2018.
+         <span className='text-[#125e51]'>Spider</span> Marketing Agency Founded in 2019.
       </h2>
 
      
           
 <ul className=" font-thin text-white   text-[14px] md:text-[17px]  ">
-  <li  data-aos-duration="1000" data-aos="fade-right" className='mt-3 fo'  >We focus on delivering high-quality, unique content that earns client trust.
+  <li  data-aos-duration="1000" data-aos="fade-right" className='mt-3 '  >
+    
+We specialize in full-service marketing, offering a wide range of services including content creation, photography, video editing, and commercial production
+
 </li>
-<li  data-aos-duration="1000" data-aos-delay="350" data-aos="fade-left" className='mt-3'>Our team of experts crafts impactful messages, builds strong brand identities,
+<li  data-aos-duration="1000" data-aos-delay="350" data-aos="fade-left" className='mt-3'>
+What sets us apart is our unique ability to blend creative content with strategic marketing, producing visually powerful campaigns that not only look great but also deliver real results.
+,
 </li>
-<li  data-aos-duration="1000" data-aos-delay="400" data-aos="fade-right" className='mt-3'>Develops effective strategies to reach your target audience.
+<li  data-aos-duration="1000" data-aos-delay="400" data-aos="fade-right" className='mt-3'>
+Our strength lies in creating trending content and captivating visuals that resonate with modern audiences. Through a seamless fusion of marketing expertise and visual storytelling
 </li>
-<li  data-aos-duration="1000" data-aos-delay="550" data-aos="fade-right" className='mt-3'>We start every project with in-depth market research to ensure your brand stands out in a competitive landscape.
+<li  data-aos-duration="1000" data-aos-delay="550" data-aos="fade-right" className='mt-3'>
+we bring brands to life in ways that are bold, engaging, and unforgettable.
 </li>
 
 </ul>
